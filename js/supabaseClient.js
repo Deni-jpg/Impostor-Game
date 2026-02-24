b37a058dba56e8532b32b18a7419840bad1d76b5
deni-jpg <soparamine23@gmail.com>
2026-02-24 10:55:56 +0000

    Atualização de anon key e ativação de RLS.

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,6 +1,4 @@
-import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm'
+import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
+import { sbURL, sbKEY } from './config.js';
 
-const supabaseUrl = "https://euqtttqkxrgdpjfruxmd.supabase.co";
-const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImV1cXR0dHFreHJnZHBqZnJ1eG1kIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTU4OTQyNzEsImV4cCI6MjA3MTQ3MDI3MX0.CEylCj2rSiASA1BZhnUBhAhVnjXNQzRB3aXsLGkkYyA";
-
-export const supabase = createClient(supabaseUrl, supabaseKey)
+export const supabase = createClient(sbURL, sbKEY)
