6a60e4099669c5d25834aaa402f7ab8a350fd369
deni-jpg <soparamine23@gmail.com>
2026-02-24 11:45:21 +0000

    Vite instalado e configuração de .env.

--- js/supabaseClient.js
+++ js/supabaseClient.js
@@ -1,4 +1,6 @@
 import { createClient } from 'https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm';
-import { sbURL, sbKEY } from './config.js';
+
+const sbURL = import.meta.env.VITE_SUPABASE_URL;
+const sbKEY = import.meta.env.VITE_SUPABASE_KEY;
 
 export const supabase = createClient(sbURL, sbKEY)
